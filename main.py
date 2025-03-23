cbd2ea4910c010fbc593f31e0df72e8357da4d55
Elijah J. <dead.knuckle@gmail.com>
2025-03-23 14:02:13 -0400

    init commit

---
+++ main.py
@@ -0,0 +1,8 @@
+"""
+For this project I'd like to build a program that will control what is being displayed 
+on two screens.
+
+One screen will be a PDF Reader, it simply opens a PDF file, reads it aloud, and displays what is currently being read.
+
+The other screen will showcase a slideshow of images that go along with the PDF.
+"""
